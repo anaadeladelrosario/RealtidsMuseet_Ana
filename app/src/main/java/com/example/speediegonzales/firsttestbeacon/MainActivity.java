cefd986024abe201b07c44fe5794c5fd7a1fc006
Cecilia To <cecilia_to2@yahoo.com>
2017-12-04 10:00:14 +0100

    Added MuseumBeacon class to be used with the arraylist. Added checks to compare distance of the beacons.

--- app/src/main/java/com/example/speediegonzales/firsttestbeacon/MainActivity.java
+++ app/src/main/java/com/example/speediegonzales/firsttestbeacon/MainActivity.java
@@ -1,21 +1,30 @@
+/**
+ * 1) Hampus 2017-11-30: 1st checkin with initial setup where Hampus able to get beacon signals and display them
+ * 2) Cecilia 2017-11-30: beaconOne(), beaconTwo(): simple print messages
+ * 3) Cecilia 2017-12-03: create MuseumBeacon class in order to track beacons in an arraylist
+ */
+
+
 package com.example.speediegonzales.firsttestbeacon;
 
-        import android.Manifest;
-        import android.os.Handler;
-        import android.os.RemoteException;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.text.method.ScrollingMovementMethod;
-        import android.util.Log;
-        import android.widget.TextView;
+import android.Manifest;
+import android.os.RemoteException;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.widget.TextView;
 
-        import org.altbeacon.beacon.Beacon;
-        import org.altbeacon.beacon.BeaconConsumer;
-        import org.altbeacon.beacon.BeaconManager;
-        import org.altbeacon.beacon.BeaconParser;
-        import org.altbeacon.beacon.RangeNotifier;
-        import org.altbeacon.beacon.Region;
-        import java.util.Collection;
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 
 
 public class MainActivity extends AppCompatActivity implements BeaconConsumer{
@@ -24,6 +33,7 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer{
     private static final String IBEACON_LAYOUT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24";
     private static final String TAG = "RangingActivity";
     private TextView logWindow;
+    private ArrayList<MuseumBeacon> beaconsList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,23 +57,31 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer{
 
     @Override
     public void onBeaconServiceConnect() {
+
         beaconManager.addRangeNotifier(new RangeNotifier() {
+
             @Override
-            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+            public void didRangeBeaconsInRegion(final Collection<Beacon> beacons, Region region) {
+
                 if (beacons.size() > 0) {
                     String beaconsFound = (Integer.toString(beacons.size()) + " beacons found");
                     String BeaconText = "";
-                    //Beacon b = beacons.iterator().next();
+
                     for(Beacon b : beacons){
-                        // BeaconText += "Beacon with the BluetoothAddress " + b.getBluetoothAddress() + " is " +b.getDistance()+" meters away \n\n";
-                        if(b.getBluetoothAddress().equals("E6:1C:EB:07:A0:BD")) {
-                            BeaconText = beaconOne(BeaconText, b);
+                        if(b.getDistance() < beacons.iterator().next().getDistance()){
+                         BeaconText += "B distance " + b.getDistance() + " where next" + beacons.iterator().next().getDistance()+" meters away \n\n";
+                         BeaconText = beaconOne(BeaconText, b);
+
+                        } else if(beacons.iterator().next().getDistance() < b.getDistance()){
+                            BeaconText += "B next distance " + beacons.iterator().next().getDistance() + " where original" + b.getDistance()+" meters away \n\n";
+                            BeaconText = beaconTwo(BeaconText, beacons.iterator().next());
                         } else
-                            BeaconText = beaconTwo(BeaconText, b);
+                            BeaconText = beaconThree(BeaconText, b);
                     }
                     clearAndPrintToScreenTextArea(beaconsFound + "\n\n" + BeaconText);
                     printToLogI(beaconsFound + "\n\n" + BeaconText);
                 }
+
             }
         });
 
@@ -77,19 +95,46 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer{
     }
 
     private String beaconOne(String BeaconText, Beacon beacon){
+        MuseumBeacon museumBeacon = null;
+            if(!beaconsList.contains(beacon.getBluetoothAddress())){
+                //check if beacon is already in beaconslist, if not, add beacon
+                museumBeacon = new MuseumBeacon(beacon);
+                beaconsList.add(museumBeacon);
+            }
+
         BeaconText += "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ \n\n" +
                 " Beacon with the BluetoothAddress " + beacon.getBluetoothAddress() + " is " +beacon.getDistance()+" meters away \n\n" +
-                " Message from ROGER RABBIT \n\n @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ \n\n\n";
+                " Message from ROGER RABBIT \n\n" + museumBeacon.toString() + "\n @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ \n\n\n";
         return BeaconText;
     }
 
     private String beaconTwo(String BeaconText, Beacon beacon){
+        MuseumBeacon museumBeacon = null;
+        if(!beaconsList.contains(beacon.getBluetoothAddress())){
+            //check if beacon is already in beaconslist, if not, add beacon
+            museumBeacon = new MuseumBeacon(beacon);
+            beaconsList.add(museumBeacon);
+        }
+
         BeaconText += "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% \n\n" +
                 " Beacon with the BluetoothAddress " + beacon.getBluetoothAddress() + " is " +beacon.getDistance()+" meters away \n\n" +
-                " Message to find NEMO \n\n %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n\n\n";
+                " Message to find NEMO \n\n " + museumBeacon.toString() + "\n %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n\n\n";
         return BeaconText;
     }
 
+    private String beaconThree(String BeaconText, Beacon beacon){
+        MuseumBeacon museumBeacon = null;
+        if(!beaconsList.contains(beacon.getBluetoothAddress())){
+            //check if beacon is already in beaconslist, if not, add beacon
+            museumBeacon = new MuseumBeacon(beacon);
+            beaconsList.add(museumBeacon);
+        }
+
+        BeaconText += "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ \n\n" +
+                " Beacon with the BluetoothAddress " + beacon.getBluetoothAddress() + " is " +beacon.getDistance()+" meters away \n\n" +
+                " Message to KERMIT THE FROG \n\n " + museumBeacon.toString() + "\n $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ \n\n\n";
+        return BeaconText;
+    }
     private void clearAndPrintToScreenTextArea(String text){
         final String textToPrint = text;
         runOnUiThread(new Runnable() {
